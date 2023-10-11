630b5b3f7b190bed3e05cfa4dda7cee91bc7862b
Wo-ogie <siwall0105@gmail.com>
2023-10-11 16:40:45 +0900

    chore: Redis 설정

---
+++ src/main/java/com/momong/backend/global/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.momong.backend.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    private final String host;
+    private final int port;
+
+    public RedisConfig(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port
+    ) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Bean
+    public RedisConnectionFactory defaultRedisConnectionFactory() {
+        return createLettuceConnectionFactory(0);
+    }
+
+    private RedisConnectionFactory createLettuceConnectionFactory(int dbIndex) {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(host);
+        redisStandaloneConfig.setPort(port);
+        redisStandaloneConfig.setDatabase(dbIndex);
+        return new LettuceConnectionFactory(redisStandaloneConfig);
+    }
+}
