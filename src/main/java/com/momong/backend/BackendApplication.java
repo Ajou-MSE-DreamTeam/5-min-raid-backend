fee889f2b0c7c02a5c586349da7324869989c95c
Wo-ogie <siwall0105@gmail.com>
2023-10-11 15:13:07 +0900

    chore: project 생성 및 초기 설정

---
+++ src/main/java/com/momong/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.momong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("classpath:/env.properties")
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
