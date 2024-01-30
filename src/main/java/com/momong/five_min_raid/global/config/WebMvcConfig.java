1c06a81a25cb047fe3ebba2c1d20e5177c9c49fd
Wo-ogie <siwall0105@gmail.com>
2024-01-30 21:17:56 +0900

    refactor: #12 `_` 및 대소문자와 상관없이 `GuardianPerType`으로 매핑되도록 convert 로직 구현

---
+++ src/main/java/com/momong/five_min_raid/global/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.momong.five_min_raid.global.config;
+
+import com.momong.five_min_raid.global.converter.GuardianPerkTypeRequestConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new GuardianPerkTypeRequestConverter());
+    }
+}
