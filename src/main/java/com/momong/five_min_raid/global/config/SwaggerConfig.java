852c151bd9ddac4a3730d30859d118b88159935a
JaeUk <siwall0105@gmail.com>
2025-02-10 16:37:49 +0900

    chore(swagger): 서버 주소 설정

--- src/main/java/com/momong/five_min_raid/global/config/SwaggerConfig.java
+++ src/main/java/com/momong/five_min_raid/global/config/SwaggerConfig.java
@@ -1,25 +1,35 @@
 package com.momong.five_min_raid.global.config;
 
+import com.momong.five_min_raid.global.common.properties.FMRaidProperties;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openApi(@Value("${backend.app.version}") String appVersion) {
+    public OpenAPI openApi(
+            @Value("${backend.app.version}") String appVersion,
+            FMRaidProperties fmRaidProperties
+    ) {
+        Server server = new Server();
+        server.setUrl(fmRaidProperties.serverUrl());
         return new OpenAPI()
                 .info(new Info()
                         .title("5 Minute Raid API Docs")
                         .description("Team Momong의 5분 레이드 API 명세서")
                         .version(appVersion))
+                .servers(List.of(server))
                 .externalDocs(new ExternalDocumentation()
                         .description("Github organization of team MoMong")
                         .url("https://github.com/Ajou-MSE-DreamTeam"))
